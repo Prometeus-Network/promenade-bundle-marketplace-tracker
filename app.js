1e1790710c13b123ec02aa34eef541d2e21ea4c3
Gosha Skryuchenkov <gosha.skryuchenkov@gmail.com>
2021-09-19 15:05:55 +0300

    ci: integrated helm

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const healthcheck = require('../promenade-thumbnail-indexer/healthcheck')
 const trackBundleMarketPlace = require('./services/bundlemarketplacetracker')
 
 trackBundleMarketPlace()
+healthcheck()
